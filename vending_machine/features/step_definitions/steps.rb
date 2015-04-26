b25d0ff0b011a344c9f7702b2c87baf667ad3118
br8b4w5 <brian_forsythe@gap.com>
2015-04-26 16:42:02 -0400

    initial step definitions

---
+++ vending_machine/features/step_definitions/steps.rb
@@ -0,0 +1,15 @@
+Given(/^item (.) costs \$(\d+\.\d+)$/) do |item_id, cost|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^(\d+) (\w+) are inserted into the coin slot$/) do |count, coins|
+  pending # express the regexp above with the code you wish you had
+end
+
+And(/^the Get\-(.) button is depressed$/) do |item_id|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^item (.) is dispensed$/) do |item_id|
+  pending # express the regexp above with the code you wish you had
+end
